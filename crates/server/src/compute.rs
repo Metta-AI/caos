0bd076b6edec7e56826535c40c85d39b8669ebc5
caos-agent <caos@caos>
2026-08-07 23:35:45 +0000

    edit

--- crates/server/src/compute.rs
+++ crates/server/src/compute.rs
@@ -789,7 +789,7 @@ fn run_image(
         .iter()
         .map(|e| {
             (
-                String::from_utf8_lossy(&e.filename.to_vec()).into_owned(),
+                String::from_utf8_lossy(e.filename.as_ref()).into_owned(),
                 e.oid.to_string(),
             )
         })
